dede26968109ba7215b16be32fdcde00ad906ca6
wickidcow <jaywago@gmail.com>
2026-08-12 12:15:39 -0400

    Remove Lombok from Drop model

--- src/main/java/dev/walshy/sfmobdrops/drops/Drop.java
+++ src/main/java/dev/walshy/sfmobdrops/drops/Drop.java
@@ -1,18 +1,49 @@
 package dev.walshy.sfmobdrops.drops;
 
+import java.util.Objects;
 import javax.annotation.Nonnull;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class Drop {
+public final class Drop {
 
     @Nonnull
     private final String slimefunItem;
     private final double chance;
     private final int amount;
+
+    public Drop(@Nonnull String slimefunItem, double chance, int amount) {
+        this.slimefunItem = Objects.requireNonNull(slimefunItem, "slimefunItem");
+        this.chance = chance;
+        this.amount = amount;
+    }
+
+    @Nonnull
+    public String getSlimefunItem() {
+        return slimefunItem;
+    }
+
+    public double getChance() {
+        return chance;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Drop other)) {
+            return false;
+        }
+        return Double.compare(chance, other.chance) == 0
+            && amount == other.amount
+            && slimefunItem.equals(other.slimefunItem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(slimefunItem, chance, amount);
+    }
 }
