9bf2768d4441a21c82b8f0ee0bcb380370b02e6e
wickidcow <jaywago@gmail.com>
2026-08-12 12:12:11 -0400

    Harden mob drops GUI for modern Paper

--- src/main/java/dev/walshy/sfmobdrops/Guis.java
+++ src/main/java/dev/walshy/sfmobdrops/Guis.java
@@ -1,5 +1,12 @@
 package dev.walshy.sfmobdrops;
 
+import dev.walshy.sfmobdrops.drops.Drop;
+import dev.walshy.sfmobdrops.drops.MobDrop;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import javax.annotation.Nonnull;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -13,71 +20,70 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import dev.walshy.sfmobdrops.drops.Drop;
-import dev.walshy.sfmobdrops.drops.MobDrop;
-
-import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-
 public final class Guis implements Listener {
 
     private static final String TITLE = ChatColor.DARK_PURPLE + "Mob Drops";
+    private static final int MAX_GUI_SIZE = 54;
 
     protected Guis() {}
 
-    // TODO: Add pages support
     public static void openMobDropList(Player player) {
         final Set<MobDrop> drops = SfMobDrops.getInstance().getMobDrops();
-        final int size = (drops.size() + 8) / 9 * 9;
+        final int requestedSize = Math.max(9, ((drops.size() + 8) / 9) * 9);
+        final int size = Math.min(MAX_GUI_SIZE, requestedSize);
 
         final Inventory inv = Bukkit.createInventory(null, size, TITLE);
-        for (MobDrop mobDrop : drops) {
-            final ItemStack is = new ItemStack(
-                mobDrop.isAllMobs()
-                    ? Material.SPAWNER
-                    : getMaterialForMob(mobDrop.getDropsFrom())
-            );
-            final ItemMeta im = is.getItemMeta();
+        int added = 0;
 
-            im.setDisplayName(mobDrop.getEntityName() != null
-                ? mobDrop.getEntityName()
-                : mobDrop.isAllMobs() ? ChatColor.GOLD + "All Mobs"
-                : getEntity(mobDrop.getDropsFrom())
+        for (MobDrop mobDrop : drops) {
+            if (added >= size) {
+                break;
+            }
+
+            final ItemStack itemStack = new ItemStack(
+                mobDrop.isAllMobs() ? Material.SPAWNER : getMaterialForMob(mobDrop.getDropsFrom())
+            );
+            final ItemMeta itemMeta = itemStack.getItemMeta();
+
+            itemMeta.setDisplayName(
+                mobDrop.getEntityName() != null
+                    ? mobDrop.getEntityName()
+                    : mobDrop.isAllMobs()
+                        ? ChatColor.GOLD + "All Mobs"
+                        : getEntity(mobDrop.getDropsFrom())
             );
 
             final List<String> lore = new ArrayList<>();
             lore.add(ChatColor.GRAY + "Drops:");
             lore.add("");
             for (Drop drop : mobDrop.getDrops()) {
-                lore.add(ChatColor.GRAY + "" + drop.getAmount() + "x "
-                    + ChatColor.GOLD + drop.getSlimefunItem()
-                );
+                lore.add(ChatColor.GRAY + "" + drop.getAmount() + "x " + ChatColor.GOLD + drop.getSlimefunItem());
                 lore.add(ChatColor.LIGHT_PURPLE + "" + drop.getChance() + "% " + ChatColor.GRAY + "chance");
                 lore.add("");
             }
 
             if (mobDrop.getEntityName() != null) {
-                lore.add(ChatColor.GRAY + "Requires name: "
-                    + ChatColor.translateAlternateColorCodes('&', mobDrop.getEntityName())
-                );
+                lore.add(ChatColor.GRAY + "Requires name: " + mobDrop.getEntityName());
             }
             if (mobDrop.getEntityNbtTag() != null) {
                 lore.add(ChatColor.GRAY + "Requires tag: " + ChatColor.LIGHT_PURPLE + mobDrop.getEntityNbtTag());
             }
 
-            im.setLore(lore);
-            is.setItemMeta(im);
+            itemMeta.setLore(lore);
+            itemStack.setItemMeta(itemMeta);
+            inv.addItem(itemStack);
+            added++;
+        }
 
-            inv.addItem(is);
+        if (drops.size() > size) {
+            player.sendMessage(
+                ChatColor.YELLOW + "Showing the first " + size + " mob drop definitions. GUI paging is not available yet."
+            );
         }
 
         player.openInventory(inv);
     }
 
-    // TODO: Get some mob heads
     private static Material getMaterialForMob(@Nonnull EntityType type) {
         if (type == EntityType.ENDER_DRAGON) {
             return Material.DRAGON_HEAD;
@@ -85,24 +91,21 @@ public final class Guis implements Listener {
             return Material.ZOMBIE_HEAD;
         } else if (type == EntityType.CREEPER) {
             return Material.CREEPER_HEAD;
-        } else {
-            final Material mat = Material.getMaterial(type + "_SPAWN_EGG");
-            if (mat != null) {
-                return mat;
-            } else {
-                return Material.SPAWNER;
-            }
         }
+
+        final Material material = Material.getMaterial(type + "_SPAWN_EGG");
+        return material != null ? material : Material.SPAWNER;
     }
 
     private static String getEntity(@Nonnull EntityType type) {
-        return ChatColor.LIGHT_PURPLE + capitalise(type.name().charAt(0) + type.name().substring(1)
-            .replace('_', ' ')
-            .toLowerCase(Locale.ROOT));
+        return ChatColor.LIGHT_PURPLE
+            + capitalise((type.name().charAt(0) + type.name().substring(1)).replace('_', ' ').toLowerCase(Locale.ROOT));
     }
 
     private static String capitalise(String str) {
-        if (str.isEmpty()) return str;
+        if (str.isEmpty()) {
+            return str;
+        }
 
         final char[] chars = str.toCharArray();
         boolean capitalizeNext = true;
@@ -119,10 +122,10 @@ public final class Guis implements Listener {
     }
 
     @EventHandler
-    public void onInvClick(@Nonnull InventoryClickEvent e) {
-        if (e.getView().getTitle().equals(TITLE)) {
-            e.setCancelled(true);
-            e.setResult(Event.Result.DENY);
+    public void onInvClick(@Nonnull InventoryClickEvent event) {
+        if (event.getView().getTitle().equals(TITLE)) {
+            event.setCancelled(true);
+            event.setResult(Event.Result.DENY);
         }
     }
 }
