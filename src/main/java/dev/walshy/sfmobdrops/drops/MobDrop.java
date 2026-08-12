8c4fc0d2c0d9b219d3b1b71b5216ac94bb4a944f
wickidcow <jaywago@gmail.com>
2026-08-12 12:15:56 -0400

    Remove Lombok from MobDrop model

--- src/main/java/dev/walshy/sfmobdrops/drops/MobDrop.java
+++ src/main/java/dev/walshy/sfmobdrops/drops/MobDrop.java
@@ -1,21 +1,15 @@
 package dev.walshy.sfmobdrops.drops;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.EntityType;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class MobDrop {
+public final class MobDrop {
 
     @Nullable
     private final EntityType dropsFrom;
@@ -26,4 +20,62 @@ public class MobDrop {
     private final NamespacedKey entityNbtTag;
     @Nonnull
     private final Set<Drop> drops;
+
+    public MobDrop(
+        @Nullable EntityType dropsFrom,
+        boolean allMobs,
+        @Nullable String entityName,
+        @Nullable NamespacedKey entityNbtTag,
+        @Nonnull Set<Drop> drops
+    ) {
+        this.dropsFrom = dropsFrom;
+        this.allMobs = allMobs;
+        this.entityName = entityName;
+        this.entityNbtTag = entityNbtTag;
+        this.drops = Collections.unmodifiableSet(new LinkedHashSet<>(Objects.requireNonNull(drops, "drops")));
+    }
+
+    @Nullable
+    public EntityType getDropsFrom() {
+        return dropsFrom;
+    }
+
+    public boolean isAllMobs() {
+        return allMobs;
+    }
+
+    @Nullable
+    public String getEntityName() {
+        return entityName;
+    }
+
+    @Nullable
+    public NamespacedKey getEntityNbtTag() {
+        return entityNbtTag;
+    }
+
+    @Nonnull
+    public Set<Drop> getDrops() {
+        return drops;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof MobDrop other)) {
+            return false;
+        }
+        return allMobs == other.allMobs
+            && dropsFrom == other.dropsFrom
+            && Objects.equals(entityName, other.entityName)
+            && Objects.equals(entityNbtTag, other.entityNbtTag)
+            && drops.equals(other.drops);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dropsFrom, allMobs, entityName, entityNbtTag, drops);
+    }
 }
